f63f0a982ba0b3c796cb4ae3c54ffe2ff8f79c6b
Benjamin Bradnick <ben.bradnick@diamond.ac.uk>
2020-04-21 10:59:05 +0100

    Update ADGenICam for Manta G145B and tidy builder.py support

--- etc/builder.py
+++ etc/builder.py
@@ -1,8 +1,7 @@
-from iocbuilder import Device, AutoSubstitution, SetSimulation
+from iocbuilder import AutoSubstitution
 from iocbuilder.arginfo import *
 
-from iocbuilder.modules.asyn import AsynPort
-from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, includesTemplates, makeTemplateInstance
+from iocbuilder.modules.ADCore import makeTemplateInstance
 from iocbuilder.modules.ADGenICam import GenICam
 
 class aravisCameraTemplate(AutoSubstitution):
@@ -14,9 +13,9 @@ class aravisCamera(GenICam):
     # This tells xmlbuilder to use PORT instead of name as the row ID
     UniqueName = "PORT"
     _SpecificTemplate = aravisCameraTemplate
-    def __init__(self,P,R,PORT, ID, CLASS, PV_ALIAS, BUFFERS = 50, MEMORY = -1, **args):
+    def __init__(self, P, R, PORT, ID, CLASS, PV_ALIAS, BUFFERS=50, MEMORY=-1, **args):
         # Init the superclass
-        self.__super.__init__(P,R,PORT,ID,CLASS,PV_ALIAS,BUFFERS,MEMORY)
+        self.__super.__init__(P, R, PORT, ID, CLASS, PV_ALIAS, BUFFERS, MEMORY)
         # Update the attributes of self from the commandline args
         self.__dict__.update(locals())
         # Make an instance of our template
@@ -26,23 +25,38 @@ class aravisCamera(GenICam):
 
     # __init__ arguments
     ArgInfo = makeArgInfo(__init__,
-        P    = Simple('PV Prefix', str),
-        R    = Simple('PV Suffix', str),
+        P       = Simple('PV Prefix', str),
+        R       = Simple('PV Suffix', str),
         PORT    = Simple('Port name for the camera', str),
         ID      = Simple('Cam ip address, hostname, MAC address, or ID <manufacturer>-<serial>, (e.g. Prosilica-02-2166A-06844)', str),
-        CLASS   = Choice('Camera class for custom commands',
-                         ["AVT_Manta_G235B","AVT_Manta_G609B","AVT_Manta_G125B","AVT_Mako_G234C","AVT_Manta_G235C","AVT_Prosilica_GC655C","AVT_Mako_1_52","Prosilica_GC","AVT_Manta_1_44","AVT_Manta_G125C","AVT_Mako_G125C","AVT_Mako_G125B","AVT_Mako_G507B","AVT_Manta_G895B","AVT_Prosilica_GT5120"]),
-        PV_ALIAS   = Choice('Use alias template to keep some key PV names the same',
-                         [0,1]),
+        CLASS   = Choice('Camera class for custom commands', [
+            'AVT_Mako_1_52',
+            'AVT_Mako_G125B',
+            'AVT_Mako_G125C',
+            'AVT_Mako_G234C',
+            'AVT_Mako_G507B',
+            'AVT_Manta_1_44',
+            'AVT_Manta_G125B',
+            'AVT_Manta_G125C',
+            'AVT_Manta_G145B',
+            'AVT_Manta_G235B',
+            'AVT_Manta_G235C',
+            'AVT_Manta_G609B',
+            'AVT_Manta_G895B',
+            'AVT_Prosilica_GC655C',
+            'AVT_Prosilica_GT5120',
+            'Prosilica_GC']),
+        PV_ALIAS= Choice('Use alias template to keep some key PV names the same',
+            [0,1]),
         BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
-                         'plugin callbacks', int),
+            'plugin callbacks', int),
         MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
-                         'for driver and all attached plugins', int))
+            'for driver and all attached plugins', int))
 
 
     def Initialise(self):
         #print "aravisConfig(const char *portName, const char *cameraName, size_t maxMemory, int priority, int stackSize)"
-        print 'aravisConfig("%(PORT)s", "%(ID)s", %(MEMORY)d, 0,1)' % self.__dict__
+        print 'aravisConfig("%(PORT)s", "%(ID)s", %(MEMORY)d, 0, 1)' % self.__dict__
 
     # Device attributes
     LibFileList = ['ADAravis']
